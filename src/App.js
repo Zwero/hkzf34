97c8607df1906be0ac25dcdc13d072f3c3dee59a
uksir <yeah126139163@163.com>
2019-08-31 15:19:21 +0800

    引入路由和ant

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component, Fragment } from 'react';
+
+import { HashRouter as Router, Link, Route } from "react-router-dom";
+
+const Home = () => <div>首页</div>
+const About = () => <div>About</div>
+const User = () => <div>User</div>
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Router>
+          <nav>
+            <Link to="/">首页</Link>
+            <Link to="/About">关于</Link>
+            <Link to="/User">用户</Link>
+          </nav>
+          <section>
+            <Route path="/" exact  component={Home} />
+            <Route path="/About" component={About} />
+            <Route path="/User" component={User} />
+          </section>
+        </Router>
+      </Fragment>
+    );
+  }
 }
-
 export default App;
