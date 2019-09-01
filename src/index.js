04bfddc1d79a6aa0aaf667516458d9b02e3f8721
uksir <yeah126139163@163.com>
2019-09-01 10:48:14 +0800

    首页-nav完成

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // 1 引入 字体图标
 import "./assets/fonts/iconfont.css";
+import "./App.css";
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import {axios  } from "../../utils/request";
+import { axios } from "../../utils/request";
 import { Carousel, WingBlank } from 'antd-mobile';
 
+import nav1 from "../../assets/images/nav-1.png";
+import nav2 from "../../assets/images/nav-2.png";
+import nav3 from "../../assets/images/nav-3.png";
+import nav4 from "../../assets/images/nav-4.png";
+
+// import "./index.module.css";
+
+import styles from "./index.module.scss";
 
 
 
@@ -9,7 +17,9 @@ import { Carousel, WingBlank } from 'antd-mobile';
 class index extends Component {
   state = {
     swiperList: [],
-    imgHeight: 176
+    imgHeight: 176,
+    // 导航数据
+    navs: [{ id: 0, title: '整租', img: nav1 }, { id: 1, title: '合租', img: nav2 }, { id: 2, title: '地图找房', img: nav3 }, { id: 3, title: '去出租', img: nav4 }]
   }
   componentDidMount() {
     axios.get("/home/swiper")
@@ -19,12 +29,12 @@ class index extends Component {
   }
 
   render() {
-    const {swiperList}=this.state;
+    const { swiperList } = this.state;
     return (
-      <div className="hk_home">
+      <div className={styles.hk_home}>
         {/* 轮播图 开始 */}
-        <div className="home_swiper">
-          {swiperList.length&& <Carousel
+        <div className={styles.home_swiper}>
+          {swiperList.length && <Carousel
             autoplay
             infinite
           >
@@ -35,7 +45,7 @@ class index extends Component {
                 style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
               >
                 <img
-                  src={'http://hkzf.zbztb.cn'+val.imgSrc}
+                  src={'http://hkzf.zbztb.cn' + val.imgSrc}
                   alt=""
                   style={{ width: '100%', verticalAlign: 'top' }}
                   onLoad={() => {
@@ -47,11 +57,22 @@ class index extends Component {
               </a>
             ))}
           </Carousel>}
-         
+
         </div>
         {/* 轮播图 结束 */}
+        {/* 导航 开始 */}
+        <nav className={styles.home_nav}>
+          {this.state.navs.map(v =>
+            <div key={v.id} className={styles.nav_item}>
+              <img src={v.img} alt="" />
+              <p>{v.title}</p>
+            </div>
+          )}
+        </nav>
+        {/* 导航 结束 */}
       </div>
     );
   }
 }
 export default index;
+
