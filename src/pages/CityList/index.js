21f7d629823f9a099e381d0e44749f84669d2dca
uksir <yeah126139163@163.com>
2019-09-03 11:42:43 +0800

    城市列表-数据处理

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -39,13 +39,53 @@ class index extends Component {
     // 1 当前城市
     const cityName = store.getState().mapReducer.cityName;
     // 2 获取所有的城市
-    const allCity=(await axios("/area/city?level=1")).body;
+    const allCity = (await axios("/area/city?level=1")).body;
     // 3 获取热门的城市
-    const hotCity=(await axios("/area/hot")).body;
-    console.log(hotCity);
-
-
-
+    const hotCity = (await axios("/area/hot")).body;
+    // 4 综合的数组
+    /* 
+    [
+      {"当前地址":["广州市"]},
+      {"热门城市":["北京","广州","上海","深圳"]}
+      {A:[]},
+      {B:[]}...
+    ]
+     */
+    let totalCity = [
+      { "当前地址": [cityName] },
+      { "热门城市": hotCity.map(v => v.label) }
+    ];
+    // 5 对数组进行排序  a aa ab abc ba bb bc ....
+    allCity.sort((a, b) => a.short.localeCompare(b.short));
+    // 6 循环
+    allCity.forEach(v => {
+      // 获取到了 “bj” 中的 "B"
+      const firstLetter = v.short[0].toUpperCase();
+      // 7 判断  获取索引 
+      const index = totalCity.findIndex(vv => {
+        // vv = { "当前地址": [cityName] }
+        // vv = { "热门城市": ['广州'] }
+        if (vv[firstLetter]) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+      // 8 判断 数组中是否存在 A 对象了
+      if (index === -1) {
+        // {A:[]} 不存在 
+        totalCity.push(
+          // firstLetter 是一个变量 而不是 对象中的一个属性
+          { [firstLetter]: [v.label] }
+        );
+      } else {
+        // totalCity 数组
+        //  totalCity[index] 数组中的某个元素  == 对象 => {"A":[]}
+        //  totalCity[index][firstLetter] {"A":[]}.A 
+        totalCity[index][firstLetter].push(v.label);
+      }
+    })
+    console.log(totalCity);
   }
 
 
