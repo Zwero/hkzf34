ef8c307785f1a6a2d395a0e623700bf1a57d25dc
uksir <yeah126139163@163.com>
2019-09-04 10:57:49 +0800

    获取城市的value

--- src/pages/BDMap/index.js
+++ src/pages/BDMap/index.js
@@ -1,9 +1,38 @@
+/* 
+1 了解百度地图的用法 
+2 根据 城市 名称 获取 id 
+3 根据 城市的 id 去获取 城市下的 房源信息 
+
+
+ */
 import React, { Component, Fragment } from 'react';
 import { NavBar, Icon } from 'antd-mobile';
 import styles from "./index.module.scss";
+import { axios } from "../../utils/request";
+import store from "../../store";
+
 const BMap = window.BMap;
 class index extends Component {
+  constructor() {
+    super();
+    store.subscribe(this.initCity);
+  }
   componentDidMount() {
+    // // 创建地图实例  
+    // const map = new BMap.Map("allmap");
+    // // 创建点坐标  
+    // // 初始化地图，设置中心点坐标和地图级别
+    // map.centerAndZoom("广州市", 15);
+    // // 缩放组件
+    // map.addControl(new BMap.NavigationControl());
+    // // 比例尺
+    // map.addControl(new BMap.ScaleControl());
+  }
+
+  async initCity() {
+    const cityName=store.getState().mapReducer.cityName;
+    console.log(store.getState());
+    console.log(cityName);
     // 创建地图实例  
     const map = new BMap.Map("allmap");
     // 创建点坐标  
@@ -13,7 +42,10 @@ class index extends Component {
     map.addControl(new BMap.NavigationControl());
     // 比例尺
     map.addControl(new BMap.ScaleControl());
+    const res1 = (await axios.get("/area/info?name="+cityName))
+    console.log(res1);
   }
+
   render() {
     return (
       <Fragment>
